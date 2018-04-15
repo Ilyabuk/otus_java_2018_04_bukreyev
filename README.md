3519b622f2b6a94f17ccc2c76bfafa63fb4ff3e5
Bukreyev Ilya <ilyabuk@gmail.com>
2018-04-15 21:33:44 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Группа 2018-04 (Java)
+
+Автор
+
+Ilya Bukreyev (Илья Букреев)
+
+ilyabuk@gmail.com
---
+++ src/main/java/ru/otus/app/App.java
@@ -0,0 +1,8 @@
+package ru.otus.app;
+
+import com.google.common.collect.Lists;
+public class App {
+    public static void main(String[] args) {
+        System.out.print("Hello!");
+    }
+}
