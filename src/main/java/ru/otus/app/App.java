494d1c9f502f36697ffb0dd103a0cb573380e340
Ilya Bukreyev <ilyabuk@gmail.com>
2018-04-17 17:36:21 +0300

    obfuscation

--- src/main/java/ru/otus/app/App.java
+++ src/main/java/ru/otus/app/App.java
@@ -2,7 +2,7 @@ package ru.otus.app;
 
 import com.google.common.collect.Lists;
 public class App {
-    public static void main(String[] args) {
-        System.out.print("Hello!");
+    public static void main(java.lang.String[] strings) {
+        System.out.print("Hello!\n");
     }
 }
---
+++ src/main/java/ru/otus/appJava8/App2.java
@@ -0,0 +1,7 @@
+package ru.otus.appJava8;
+
+public class App2 {
+    public static void main(String[] args) {
+        System.out.print("Shello!\n");
+    }
+}
